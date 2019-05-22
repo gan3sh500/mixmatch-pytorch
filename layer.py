785de0bd6fbcd73b5043131e86a6d3a64f583242
Francisco Perdigon Romero <fperdigon88@gmail.com>
2019-05-22 16:54:28 -0400

    Adding to categorical function
    Normally the PyTorch labels for classification are in integer numbers associated with classes.
    For the MixMatch algorithm, the labels need to be in one hot format.
    The to_categorical function was taken from Keras code, is able to handle batches.
    num_clases=10 for future CIFAR test.
    Inside of the algorithm y is converted to one hot encode format. 
    At the end of the MixMatch phase, the labels p and q are converted back to the natural classes format in PyTorch

--- layer.py
+++ layer.py
@@ -44,9 +44,31 @@ def label_guessing(model, ub, K):
 
     return (sum/K).cpu().detach().numpy()
 
+def to_categorical(y, num_classes=None, dtype='float32'):
+    """Converts a class vector (integers) to binary class matrix.
+    E.g. for use with categorical_crossentropy.
+    Taken from Keras code
+    https://github.com/keras-team/keras/blob/master/keras/utils/np_utils.py#L9
+    """
+
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes), dtype=dtype)
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
+
 
 def mixmatch(x, y, u, model, augment_fn, T=0.5, K=2, alpha=0.75):
     xb = augment_fn(x)
+    y = to_categorical(y, num_classes=10) # Converting to one hot encode, num_clases=10 for future CIFAR test
     ub = [augment_fn(u) for _ in range(K)]
     avg_probs = label_guessing(model, ub, K)
     qb = sharpen(avg_probs, T)
@@ -57,6 +79,10 @@ def mixmatch(x, y, u, model, augment_fn, T=0.5, K=2, alpha=0.75):
     Wy = np.concatenate([qb, y], axis=0)[indices]
     X, p = mixup_mod(xb, Wx[:len(xb)], y, Wy[:len(xb)], alpha)
     U, q = mixup_mod(Ux, Wx[len(xb):], Uy, Wy[len(xb):], alpha)
+    
+    # One hot decode for PyTorch labels compability
+    p = p.argmax(axis=1)
+    q = q.argmax(axis=1)
     return X, p, U, q
 
 
