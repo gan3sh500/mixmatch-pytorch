e14372127dc3a57d9e9ccea6425ec816e21c795e
Fco <fperdigon88@gmail.com>
2019-05-27 16:23:16 -0400

    Creating a more complex data augmentation. Some codes related where fixed in order to make it compatible with the DA library

--- mixmatch_utils.py
+++ mixmatch_utils.py
@@ -4,14 +4,44 @@ import imgaug as ia
 import imgaug.augmenters as iaa
 
 
-def get_augmentor():
-    seq = iaa.Sequential([
-        iaa.Crop(px=(0, 16)),
-        iaa.Fliplr(0.5),
-        iaa.GaussianBlur(sigma=(0, 3.0))
+def get_augmenter():
+    seq = iaa.Sequential([iaa.Fliplr(0.5), # horrizontal flips
+    iaa.Crop(percent=(0, 0.1)), # random crops
+    # Small gaussian blur with random sigma between 0 and 0.5.
+    # But we only blur about 50% of all images.
+    iaa.Sometimes(0.5,
+        iaa.GaussianBlur(sigma=(0, 0.5))
+    ),
+    # Strengthen or weaken the contrast in each image.
+    iaa.ContrastNormalization((0.75, 1.5)),
+    # Add gaussian noise.
+    # For 50% of all images, we sample the noise once per pixel.
+    # For the other 50% of all images, we sample the noise per pixel AND
+    # channel. This can change the color (not only brightness) of the
+    # pixels.
+    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+    # Make some images brighter and some darker.
+    # In 20% of all cases, we sample the multiplier once per channel,
+    # which can end up changing the color of the images.
+    iaa.Multiply((0.8, 1.2), per_channel=0.2),
+    # Apply affine transformations to each image.
+    # Scale/zoom them, translate/move them, rotate them and shear them.
+    iaa.Affine(
+        scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+        translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+        rotate=(-25, 25),
+        shear=(-8, 8)
+    )
     ])
     def augment(images):
-        return seq.augment(images.transpose(0, 2, 3, 1)).transpose(0, 2, 3, 1)
+        # Only works with list. Convert np to list
+        imgs = []
+        for i in range(images.shape[0]):
+            imgs.append(images[i,:,:,:])
+
+        images = images
+
+        return seq.augment(images=images)
     return augment
     
     
@@ -96,7 +126,7 @@ class MixMatchLoss(torch.nn.Module):
         self.mse = torch.nn.MSELoss()
         super(MixMatchLoss, self).__init__()
     
-    def forward(X, U, p, q):
+    def forward(self, X, U, p, q):
         X_ = np.concatenate([X, U], axis=1)
         y_ = np.concatenate([p, q], axis=1)
         return self.xent(preds[:len(p)], p) + self.mse(preds[len(p):], q)
